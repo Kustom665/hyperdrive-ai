55e3c18ff9232914df71dc0b612cf9b8f629d328
Will Shainin <wshainin@gmail.com>
2024-08-19 13:54:13 -0700

    Debugging updates and eval script

--- traiderdaive/ray_environments/ray_hyperdrive_env.py
+++ traiderdaive/ray_environments/ray_hyperdrive_env.py
@@ -120,7 +120,9 @@ class RayHyperdriveEnv(MultiAgentEnv):
         env_config,
     ):
         """Initializes the environment"""
+        print(f"\n\n{'-' * 100}\nINIT CALLED!\n{'-' * 100}\n\n")
         self.worker_index = env_config.worker_index
+        print(f"\n{'-' * 100}\nWorker index: {self.worker_index}\n{'-' * 100}\n\n")
 
         if env_config.get("env_config") is None:
             self.env_config = self.Config()
@@ -155,8 +157,11 @@ class RayHyperdriveEnv(MultiAgentEnv):
         initial_pool_config = LocalHyperdrive.Config()
         self.chain = LocalChain(local_chain_config)
         self.interactive_hyperdrive = LocalHyperdrive(self.chain, initial_pool_config)
+        print(f"\n{self.chain.rpc_uri}\n{self.interactive_hyperdrive.hyperdrive_address}\n")
         # TODO: This might need to be env indexed as well if num_envs_per_runner > 1
-        if self.worker_index == 1:
+
+        print(f"\n{self.eval_mode}\n")
+        if self.eval_mode:
             self.chain.run_dashboard()
 
         # TODO set seed
@@ -415,7 +420,7 @@ class RayHyperdriveEnv(MultiAgentEnv):
             # ordering.
             trade_positions = trade_positions.sort_values("maturity_time")
             num_trade_positions = len(trade_positions)
-            
+
             # Handle closing orders
             # The index of orders here is from oldest to newest
             # TODO (sheng) if we want the rl bot to explicitly learn how to
@@ -471,26 +476,24 @@ class RayHyperdriveEnv(MultiAgentEnv):
                         agent_wallet_balance = self.rl_agents[agent_id].get_wallet().balance.amount
                         if trade_type == TradeTypes.LONG and agent_wallet_balance >= min_tx_amount:
                             # Get the agent's max trade amount
-                            max_long_amount = self.interactive_hyperdrive.interface.calc_max_long(budget=agent_wallet_balance)
+                            max_long_amount = self.interactive_hyperdrive.interface.calc_max_long(
+                                budget=agent_wallet_balance
+                            )
                             # While volume isn't strictly a probability, we interpret it as a value between 0 and 1
                             amount_probability = FixedPoint(expit(open_long_short_actions[trade_type.value, 1]))
                             # Map the probability to be between the min and max transaction amounts
-                            volume_adjusted = (
-                                min_tx_amount
-                                +  amount_probability * (max_long_amount - min_tx_amount)
-                            )
+                            volume_adjusted = min_tx_amount + amount_probability * (max_long_amount - min_tx_amount)
                             self.rl_agents[agent_id].open_long(base=volume_adjusted)
                         elif trade_type == TradeTypes.SHORT and agent_wallet_balance >= min_tx_amount:
                             # Get the agent's max trade amount
                             agent_wallet_balance = self.rl_agents[agent_id].get_wallet().balance.amount
-                            max_short_amount = self.interactive_hyperdrive.interface.calc_max_short(budget=agent_wallet_balance)
+                            max_short_amount = self.interactive_hyperdrive.interface.calc_max_short(
+                                budget=agent_wallet_balance
+                            )
                             # While volume isn't strictly a probability, we interpret it as a value between 0 and 1
                             amount_probability = FixedPoint(expit(open_long_short_actions[trade_type.value, 1]))
                             # Map the probability to be between the min and max transaction amounts
-                            volume_adjusted = (
-                                min_tx_amount
-                                + amount_probability  * (max_short_amount - min_tx_amount)
-                            )
+                            volume_adjusted = min_tx_amount + amount_probability * (max_short_amount - min_tx_amount)
                             self.rl_agents[agent_id].open_short(bonds=volume_adjusted)
                     # Base exception here to catch rust errors
                     except BaseException as err:  # pylint: disable=broad-except
@@ -502,14 +505,18 @@ class RayHyperdriveEnv(MultiAgentEnv):
         lp_actions_expit = expit(lp_actions)
         if agent_wallet.balance.amount >= min_tx_amount:
             add_lp_probability = lp_actions_expit[0]
-            add_lp_volume = min_tx_amount + FixedPoint(lp_actions_expit[1]) * (agent_wallet.balance.amount - min_tx_amount)
+            add_lp_volume = min_tx_amount + FixedPoint(lp_actions_expit[1]) * (
+                agent_wallet.balance.amount - min_tx_amount
+            )
         else:
             add_lp_probability = np.float64(0)
             add_lp_volume = FixedPoint(0)
 
         if agent_wallet.lp_tokens >= min_tx_amount:
             remove_lp_probability = lp_actions_expit[2]
-            remove_lp_volume = min_tx_amount + FixedPoint(lp_actions_expit[3]) * (agent_wallet.lp_tokens - min_tx_amount)
+            remove_lp_volume = min_tx_amount + FixedPoint(lp_actions_expit[3]) * (
+                agent_wallet.lp_tokens - min_tx_amount
+            )
         else:
             remove_lp_probability = np.float64(0)
             remove_lp_volume = FixedPoint(0)
@@ -588,6 +595,7 @@ class RayHyperdriveEnv(MultiAgentEnv):
         # Do actions and get truncated status for agents provided, and set the rest to True
         self.interactive_hyperdrive.sync_database()
 
+        print(f"Action dict: {action_dict}")
         for agent_id, action in action_dict.items():
             _ = self._apply_action(agent_id, action)
 
@@ -616,6 +624,7 @@ class RayHyperdriveEnv(MultiAgentEnv):
             self.interactive_hyperdrive.set_variable_rate(new_rate)
 
         self.interactive_hyperdrive.sync_database()
+        print(f"{self.interactive_hyperdrive.get_trade_events()}")
 
         observations = self._get_observations(agents=action_dict.keys())
         info = self._get_info(agents=action_dict.keys())
@@ -731,3 +740,6 @@ class RayHyperdriveEnv(MultiAgentEnv):
     def render(self) -> None:
         """Renders the environment. No rendering available for hyperdrive env."""
         return None
+
+    def __del__(self):
+        self.chain.cleanup()
